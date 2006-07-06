b9c361316f2a2b324606bc88cbf0ad0ea932bfdd
Gustavo Sverzut Barbieri <barbieri@gmail.com>
2006-07-06 03:47:19 +0000

    Table now supports Images!

---
+++ tests/table-images.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+
+from eagle import *
+
+
+App( title="Table with Images",
+     center=Table( id="t1",
+                   label=None,
+                   editable=True,
+                   items=( ( Image( filename="test.png" ), "bla" ),
+                           ( Image( filename="test.png" ), "123" ),
+                           ),
+                   ),
+     )
+
+run()
