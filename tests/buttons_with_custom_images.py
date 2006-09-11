aa48d6a7007e4694d254f7e652d4003fd077d589
Gustavo Sverzut Barbieri <barbieri@gmail.com>
2006-09-11 01:19:24 +0000

    callback to notify available canvas size

--- tests/buttons_with_custom_images.py
+++ tests/buttons_with_custom_images.py
@@ -2,15 +2,36 @@
 
 from eagle import *
 
-App(title="Test of buttons with images",
-    center=(Button(id="b0",
-                   label="my label",
-                   image="test.png"),
-            Button(id="b1",
-                   label="my label",
-                   image=Image(filename="test.png")),
-            Button(id="b2",
-                   label=None,
-                   image="test.png")))
+data = chr(128) * ( 30 * 30 * 3 )
+img = Image( width=30, height=30, depth=24,
+             data=data)
+
+def change_b3( app, button ):
+    if button.image == "test.png":
+        button.image = img
+    else:
+        button.image = "test.png"
+
+
+App( title="Test of buttons with images",
+     center=( Button( id="b0",
+                      label="my label",
+                      image="test.png",
+                      ),
+              Button( id="b1",
+                      label="my label",
+                      image=Image( filename="test.png" ),
+                      ),
+              Button( id="b2",
+                      label=None,
+                      image="test.png",
+                      ),
+              Button( id="b3",
+                      label="Change-me",
+                      image="test.png",
+                      callback=change_b3,
+                      ),
+              )
+     )
 
 run()
---
+++ tests/canvas-resize.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+
+from eagle import *
+
+def resize_canvas( app, canvas, width, height ):
+    canvas.resize( width, height )
+    canvas.clear()
+    canvas.draw_text( "size: %dx%d" % ( width, height ), fgcolor="white" )
+
+App( title="Resize Canvas",
+     center=Canvas( id="c0",
+                    label=None,
+                    scrollbars=None,
+                    width=200,
+                    height=300,
+                    resize_callback=resize_canvas,
+                    ),
+     )
+
+run()
