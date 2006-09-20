0b2a99100f580f4496698cc26bdea44b552e5b62
Gustavo Sverzut Barbieri <barbieri@gmail.com>
2006-09-20 03:39:04 +0000

    function to enable/disable window decorations

---
+++ tests/test-window-decorated.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+
+from eagle import App, CheckBox, run
+
+def toggle_decoration( app, cb, value ):
+    app.window_decorated = value
+# toggle_decoration()
+
+
+App( title="Change Window Decoration",
+     window_decorated=False,
+     center=CheckBox( id="cb",
+                      label="Window is decorated",
+                      callback=toggle_decoration,
+                      ),
+     )
+run()
